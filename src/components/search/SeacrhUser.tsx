e65a8f2c94e99e589c4cfd0a58f97805684a2e09
tanya-kostecki <tanya.pavlova1294@gmail.com>
2024-01-30 13:37:25 +0100

    corrections

--- src/components/search/SeacrhUser.tsx
+++ src/components/search/SeacrhUser.tsx
@@ -2,17 +2,25 @@ import * as S from './search.styles'
 
 interface Modal {
   setInput: (input: string) => void;
+  searchUser: string;
+  setSearchUser: (searchUser: string) => void
 }
-const SeacrhUser = ({ setInput }: Modal) => {
+const SeacrhUser = ({ setInput, searchUser, setSearchUser }: Modal) => {
+  
+  const handleUserSearch = () => {
+    setInput(searchUser)
+  }
   return (
     <S.SearchContainer>
       <S.GitHubLogo src="github-logo.png" />
       <S.SearchTitle>Поиск пользователя GitHub</S.SearchTitle>
       <S.SearchForm>
         <S.SearchInput
+          value={searchUser}
           placeholder="Введите имя пользователя"
-          onChange={(e) => setInput(e.target.value)}
+          onChange={(e) => setSearchUser(e.target.value)}
         />
+        <S.SearchButton onClick={handleUserSearch}>Найти</S.SearchButton>
       </S.SearchForm>
     </S.SearchContainer>
   );
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,31 +1,43 @@
-import { useState } from 'react'
-import SeacrhUser from '../../components/search/SeacrhUser'
-import UserInfo from '../../components/modal/UserInfo'
-import { useGetUsersQuery } from '../../redux/services/users'
-import Users from '../../components/users/Users'
-import { MainContainer } from './main.styles'
+import { useEffect, useState } from 'react';
+import SeacrhUser from '../../components/search/SeacrhUser';
+import UserInfo from '../../components/modal/UserInfo';
+import { useLazyGetUsersQuery } from '../../redux/services/users';
+import Users from '../../components/users/Users';
+import { MainContainer } from './main.styles';
 import { User } from '../../interfaces/interfaces';
 import RadioButtons from '../../components/radio-buttons/RadioButtons';
+import { Search } from '../../interfaces/interfaces';
 
 const MainPage = () => {
   const [isModal, setIsModal] = useState<boolean>(false);
 
-  const [input, setInput] = useState<string>('github');
+  const [input, setInput] = useState<string>('');
+
+  const [searchUser, setSearchUser] = useState<string>('');
 
   const [order, setOrder] = useState<string>('desc');
 
   const [pagination, setPagination] = useState<number>(1);
 
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useGetUsersQuery({
-    login: input,
-    order: order,
-    per_page: 30,
-    page: pagination,
-  });
+  const [users, setUsers] = useState<Search>();
+
+  const [getUsers, { error, isLoading }] = useLazyGetUsersQuery();
+
+  useEffect(() => {
+    if (!input) {
+      return;
+    } else {
+      try {
+        getUsers({ login: input, order: order, per_page: 30, page: pagination })
+          .unwrap()
+          .then((data: Search) => {
+            setUsers(data);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [order, pagination, input]);
 
   const [selectedUser, setSelectedUser] = useState<User>({
     login: '',
@@ -36,7 +48,11 @@ const MainPage = () => {
 
   return (
     <MainContainer>
-      <SeacrhUser setInput={setInput} />
+      <SeacrhUser
+        setInput={setInput}
+        searchUser={searchUser}
+        setSearchUser={setSearchUser}
+      />
       {isModal && <UserInfo setIsModal={setIsModal} user={selectedUser} />}
       {error ? (
         <div>Что-то пошло не так, попробуйте через несколько минут</div>
@@ -46,20 +62,28 @@ const MainPage = () => {
             <div>Пользователи загружаются...</div>
           ) : (
             <>
-              <RadioButtons order={order} setOrder={setOrder} />
-              <Users
-                users={users}
-                setIsModal={setIsModal}
-                setSelectedUser={setSelectedUser}
-                pagination={pagination}
-                setPagination={setPagination}
-              />
+              {!error && input.length > 2 && users?.items.length !== 0 ? (
+                <>
+                  <RadioButtons order={order} setOrder={setOrder} />
+                  <Users
+                    users={users}
+                    setIsModal={setIsModal}
+                    setSelectedUser={setSelectedUser}
+                    pagination={pagination}
+                    setPagination={setPagination}
+                  />
+                </>
+              ) : null}
+
+              {input.length > 0 && users?.items.length === 0 && !error && (
+                <div>Пользователи не найдены </div>
+              )}
             </>
           )}
         </>
       )}
     </MainContainer>
   );
-}
+};
 
-export default MainPage
+export default MainPage;
